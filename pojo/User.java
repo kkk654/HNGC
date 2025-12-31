f90d980e2ac9096fdf7fbcf33e1034e6523d2061
无敌的管子 <480124350@qq.com>
2025-12-31 17:09:28 +0800

    Create User.java

---
+++ pojo/User.java
@@ -0,0 +1,29 @@
+package com.qf.pojo;
+
+/**
+ * @author miaokaidi
+ * @date 2024/10/14/4:32
+ * @desc
+ */
+
+public class User {
+    private  String userName;
+
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
